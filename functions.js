e87a0a4a696f8344de2534e78c8823c6e08fdac2
priya singh <ps597934@gmail.com>
2022-06-22 16:38:11 +0530

    function

---
+++ functions.js
@@ -0,0 +1,39 @@
+// NORMAL FUNCTION
+ function (functionName) (parameter){    DECLARATION
+
+    return()                              DEFINITION
+}
+
+// ARROW FUNCTION
+let (functionName) = (parameter)=>{
+
+    return()
+}
+
+// IIFE FUNCTION 
+(function (functionName) (parameter){
+
+    return()
+})(parametervalue)
+
+// ANONYMCUS FUNCTION
+
+function (parameter){
+
+    return()
+}
+
+// SETINTERVAL
+const message = function(){
+    console.log("this message is show after 5 second");
+};
+
+setinterval(message, 5000);
+
+// SETTIMEOUT
+
+const message = function (){
+    console.log("this message is shown after 5 second");
+};
+
+setTimeout(message, 5000);
